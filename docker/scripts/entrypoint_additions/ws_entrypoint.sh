bba753710009fadebc25bca21acd1c2a384689c6
Jemish Ghoghari <jemishghoghari50@gmail.com>
2025-10-22 01:22:25 +0200

    removed unnecessory commands

--- docker/scripts/entrypoint_additions/ws_entrypoint.sh
+++ docker/scripts/entrypoint_additions/ws_entrypoint.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# add user to additional groups
-adduser ${USERNAME} zed >/dev/null
-adduser ${USERNAME} dialout >/dev/null
-
-if [[ -n "$GPIO_GID" && "$GPIO_GID" =~ ^[0-9]+$ ]]; then
-    if ! getent group ${GPIO_GID} > /dev/null; then
-        groupadd -g ${GPIO_GID} gpio
-    fi
-    adduser ${USERNAME} gpio >/dev/null
-fi
-
 # source ROS and Isaac ROS workspace
 source /opt/ros/${ROS_DISTRO}/setup.bash
 source ${ISAAC_ROS_WS}/install/setup.bash
