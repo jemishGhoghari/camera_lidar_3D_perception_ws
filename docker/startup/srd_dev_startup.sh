350e3c4fabac9a1cd6b18bb56d50c7238feecada
Jemish Ghoghari <jemishghoghari50@gmail.com>
2025-10-22 01:23:01 +0200

    fix workspace path

--- docker/startup/srd_dev_startup.sh
+++ docker/startup/srd_dev_startup.sh
@@ -2,5 +2,5 @@
 
 # run isaac_ros docker without building
 $HOME/workspaces/srd_acu_ws/src/isaac_ros_common/scripts/run_dev.sh \
---isaac_ros_dev_dir $HOME/workspaces/aici_challenge_ws \
+--isaac_ros_dev_dir $HOME/workspaces/aici_gmbh_robotics_ws \
 $@
