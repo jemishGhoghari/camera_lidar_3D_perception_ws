114b8180edd63b17b71f829cbb42fb2ba5568d7c
Jemish Ghoghari <jemishghoghari50@gmail.com>
2025-10-28 02:53:15 +0100

    refactor launch file for improved readability and organization; update launch arguments and parameter handling

--- src/camera_lidar_3d_perception/launch/camera_lidar_perception_node.launch.py
+++ src/camera_lidar_3d_perception/launch/camera_lidar_perception_node.launch.py
@@ -2,30 +2,35 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, GroupAction, OpaqueFunction
 from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-from launch_ros.actions import ComposableNodeContainer, PushRosNamespace, LoadComposableNodes
+from launch_ros.actions import (
+    ComposableNodeContainer,
+    PushRosNamespace,
+    LoadComposableNodes,
+)
 from launch_ros.descriptions import ComposableNode
 from launch_ros.substitutions import FindPackageShare
 
+
 def launch_setup(context, *args, **kwargs):
 
-    component_container_name = LaunchConfiguration('component_container_name')
-    standalone = LaunchConfiguration('standalone')
-    pointcloud_topic = LaunchConfiguration('pointcloud_topic')
-    image_input_topic = LaunchConfiguration('detection_2d_input_topic')
-    detections_output_topic = LaunchConfiguration('detections_3d_output_topic')
-    camera_info_topic = LaunchConfiguration('camera_info_topic')
-
-    classes_config = PathJoinSubstitution(
-        [FindPackageShare('camera_lidar_3d_perception'), 'config', 'config.yaml']
-    )
+    component_container_name = LaunchConfiguration("component_container_name")
+    standalone = LaunchConfiguration("standalone")
+    pointcloud_topic = LaunchConfiguration("pointcloud_topic")
+    image_input_topic = LaunchConfiguration("detection_2d_input_topic")
+    detections_output_topic = LaunchConfiguration("detections_3d_output_topic")
+    camera_info_topic = LaunchConfiguration("camera_info_topic")
+    voxel_leaf_size = LaunchConfiguration("voxel_leaf_size")
+    cluster_tolerance = LaunchConfiguration("cluster_tolerance")
+    min_cluster_size = LaunchConfiguration("min_cluster_size")
+    max_cluster_size = LaunchConfiguration("max_cluster_size")
 
     component_container = ComposableNodeContainer(
-        package='rclcpp_components',
-        executable='component_container_mt',
-        namespace='',
+        package="rclcpp_components",
+        executable="component_container_mt",
+        namespace="",
         name=component_container_name,
-        arguments=['--ros-args', '--log-level', 'INFO'],
-        output='screen',
+        arguments=["--ros-args", "--log-level", "INFO"],
+        output="screen",
         condition=IfCondition(standalone),
     )
 
@@ -34,24 +39,27 @@ def launch_setup(context, *args, **kwargs):
         plugin="camera_lidar_3d_perception::CameraLidarPerceptionNode",
         name="camera_lidar_3d_perception_node",
         parameters=[
-            # classes_config,
+            {
+                "voxel_leaf_size": voxel_leaf_size,
+                "cluster_tolerance": cluster_tolerance,
+                "min_cluster_size": min_cluster_size,
+                "max_cluster_size": max_cluster_size,
+            }
         ],
         remappings=[
-            ('pointcloud', pointcloud_topic),
-            ('detections', image_input_topic),
-            ('camera_info', camera_info_topic),
-            ('detections_3d', detections_output_topic)
+            ("pointcloud", pointcloud_topic),
+            ("detections", image_input_topic),
+            ("camera_info", camera_info_topic),
+            ("detections_3d", detections_output_topic),
         ],
         extra_arguments=[{"use_intra_process_comms": True}],
     )
 
     load_composable_nodes_action = GroupAction(
         [
-            PushRosNamespace(''),
+            PushRosNamespace(""),
             LoadComposableNodes(
-                composable_node_descriptions=[
-                    camera_lidar_detection_node
-                ],
+                composable_node_descriptions=[camera_lidar_detection_node],
                 target_container=component_container_name,
             ),
         ]
@@ -59,37 +67,58 @@ def launch_setup(context, *args, **kwargs):
 
     return [component_container, load_composable_nodes_action]
 
+
 def generate_launch_description():
     launch_args = [
         DeclareLaunchArgument(
-            'component_container_name',
-            default_value='camera_lidar_perception_container',
-            description='The name of the component container',
+            "component_container_name",
+            default_value="camera_lidar_perception_container",
+            description="The name of the component container",
         ),
         DeclareLaunchArgument(
-            'standalone',
-            default_value='True',
-            description='Whether the component container should be launched or not',
+            "standalone",
+            default_value="True",
+            description="Whether the component container should be launched or not",
         ),
         DeclareLaunchArgument(
-            'pointcloud_topic',
-            default_value='pointcloud',
-            description='The topic to subscribe to for the LiDAR Pointcloud',
+            "pointcloud_topic",
+            default_value="pointcloud",
+            description="The topic to subscribe to for the LiDAR Pointcloud",
         ),
         DeclareLaunchArgument(
-            'detection_2d_input_topic',
-            default_value='detections',
-            description='The topic to subscribe to for the 2D Detections',
+            "detection_2d_input_topic",
+            default_value="detections",
+            description="The topic to subscribe to for the 2D Detections",
         ),
         DeclareLaunchArgument(
-            'detections_3d_output_topic',
-            default_value='detections_3d',
-            description='the topic to publish the 3D Detections',
+            "detections_3d_output_topic",
+            default_value="detections_3d",
+            description="the topic to publish the 3D Detections",
         ),
         DeclareLaunchArgument(
-            'camera_info_topic',
-            default_value='camera_info',
-            description='The topic to subscribe to for the Camera Info',
+            "camera_info_topic",
+            default_value="camera_info",
+            description="The topic to subscribe to for the Camera Info",
+        ),
+        DeclareLaunchArgument(
+            "voxel_leaf_size",
+            default_value="0.1",
+            description="The size of the voxel grid leaf for downsampling the pointcloud",
+        ),
+        DeclareLaunchArgument(
+            "cluster_tolerance",
+            default_value="0.1",
+            description="The tolerance for clustering points in the pointcloud",
+        ),
+        DeclareLaunchArgument(
+            "min_cluster_size",
+            default_value="10",
+            description="The minimum size of a cluster to be considered valid",
+        ),
+        DeclareLaunchArgument(
+            "max_cluster_size",
+            default_value="25000",
+            description="The maximum size of a cluster to be considered valid",
         ),
     ]
 
